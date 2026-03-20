96273564b01e9301667e2f99927c42d756f1b42f
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-19 22:37:22 -0400

    initial set up

---
+++ lab2.js
@@ -0,0 +1,19 @@
+// TASK 1: ARRAY MANIPULATION
+
+//declared an array to a variable
+let shoppingList = [];
+
+//function that takes an item as a parameter and adds it to the shoppingList array
+const addItem = (item) =>{
+    return shoppingList.push(item);
+}
+
+//function called removeLastItem that removes the last item from the shoppingList array
+const removeLastItem = ()=>{
+    return shoppingList.pop();
+}
+
+//a function called displayList that logs all items in the shoppingList array to the console.
+ const displayList = ()=>{
+    console.log(shoppingList)
+ }
