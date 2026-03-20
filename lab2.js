a6e625400ca4441c68c274fb9b4805445dd6ae6c
Robb-Designs <rtlee@aggies.ncat.edu>
2026-03-19 23:45:43 -0400

    Task 2 complete

--- lab2.js
+++ lab2.js
@@ -4,9 +4,9 @@
 let shoppingList = [];
 
 //function that takes an item as a parameter and adds it to the shoppingList array
-const addItem = (item) =>{
-    return shoppingList.push(item);
-}
+// const addItem = (item) =>{
+//     return shoppingList.push(item);
+// }
 
 //function called removeLastItem that removes the last item from the shoppingList array
 const removeLastItem = ()=>{
@@ -17,13 +17,37 @@ const removeLastItem = ()=>{
  const displayList = ()=>{
     console.log(shoppingList)
  }
-
-
  //clg testing
- addItem("Apples");
- addItem("Peaches");
- addItem("Chocolate Milk");
- removeLastItem()
- displayList()
+//  addItem("Apples");
+//  addItem("Peaches");
+//  addItem("Chocolate Milk");
+//  removeLastItem()
+//  displayList()
 
- console.log(shoppingList);
+//  console.log(shoppingList);
+
+
+
+
+
+
+ // TASK 2: FILTER AND SEARCH
+
+ //Modify the addItem function to only add the item if it is not already in the shoppingList array.
+ const addItem = (item) =>{
+    if(!shoppingList.includes(item)){
+    return shoppingList.push(item);
+    }else{
+        console.log(`${item} is already on your list`)
+    }
+}
+
+// a function called filterItems that takes a search term as a parameter and returns all items in the shoppingList that contain that search term (case-insensitive)
+const filterItems = (searchTerm) => {
+    return shoppingList.filter(item =>
+        item.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+};
+
+
+console.log(filterItems("milk"))
